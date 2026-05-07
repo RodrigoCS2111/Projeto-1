c3f6770a009fe6380767a5851315e12e26cd1b02
Eduardo Américo Bezerra da Silva <166758614+EduardoAmericoo@users.noreply.github.com>
2026-05-07 20:10:10 -0300

    inclusão dos cálculos dos veículos a combustão

--- EcoTransition/calculos.py
+++ EcoTransition/calculos.py
@@ -1,2 +1,19 @@
 # Responsável pelos cálculos do sistema.
 # Aqui ficarão as fórmulas de economia, custo, CO₂, payback, score e recomendação.
+
+def calcular_combustao(quilometragem_mensal, consumo_km_l, preco_gasolina, ipva, seguro, manutencao_anual_combustao, anos):
+
+    litros_por_mes = quilometragem_mensal / consumo_km_l
+
+    custo_combustivel_mensal = litros_por_mes * preco_gasolina
+
+    custo_combustivel_anual = custo_combustivel_mensal * 12
+
+    custo_total_combustao = (custo_combustivel_anual + ipva + seguro + manutencao_anual_combustao) * anos
+
+    return {
+        "litros_por_mes": litros_por_mes,
+        "custo_combustivel_mensal": custo_combustivel_mensal,
+        "custo_combustivel_anual": custo_combustivel_anual,
+        "custo_total_combustao": custo_total_combustao
+    }
